9f21efb1cd14c7275944af3b0b2a80985a760688
nicholasc861 <31120213+nicholasc861@users.noreply.github.com>
2020-01-13 22:59:01 -0500

    data successfully extracted from json

--- main.go
+++ main.go
@@ -11,48 +11,46 @@ import (
 )
 
 type Item struct {
-	ItemName     string  `json:"name"`
-	ItemDesc     string  `json:"description"`
-	ItemPrice    float64 `json:"price"`
-	ShippingCost float64 `json:"priceCurrency"`
-	ItemCond     string  `json:"itemCondition"`
+	ItemName     string `json:"name"`
+	ItemDesc     string `json:"description"`
+	ItemPrice    string `json:"price"`
+	ShippingCost string `json:"priceCurrency"`
+	ItemCond     string `json:"itemCondition"`
 }
 
 func main() {
-
+	fetch_all_items()
 }
 
-func fetch_all_items(searchTerm string) {
-	c := colly.NewCollector(
-		colly.AllowedDomains("mercari.com"),
-		colly.Async(true),
-	)
+func fetch_all_items() {
+	c := colly.NewCollector()
+	//colly.Async(true),
 
 	c.OnHTML("div.Flex-ych44r-0.Space-cutht5-0.Container-sc-9aa7mx-0.hepKGV", func(e *colly.HTMLElement) {
-		e.ForEach("div.Flex-ych44r-0.Space-cutht5-0.Container-sc-9aa7mx-0.withMetaInfo__FullContainer-sc-1j2k5ln-0.hyLExl > script:first-of-type", func(_ int, e *colly.HTMLElement) {
-			dat := e.Text
+		e.ForEach("div.Flex-ych44r-0.Space-cutht5-0.Container-sc-9aa7mx-0.withMetaInfo__FullContainer-sc-1j2k5ln-0.hyLExl", func(_ int, e *colly.HTMLElement) {
+			dat := e.ChildText("script")
 
-			jsonData := dat[strings.Index(dat, "{") : len(dat)-1]
+			jsonData := dat[strings.Index(dat, "{"):len(dat)]
 			i := &Item{}
 			err := json.Unmarshal([]byte(jsonData), i)
+
 			if err != nil {
 				log.Fatal(err)
 			}
 
 			name := i.ItemName
 
-			fmt.Printf("Product Name: %s", name)
+			fmt.Printf("Product Name: %s \n", name)
 		})
 	})
 
 	//test := "Nike"
 	//test1 := urlBuilderQuery(test).String()
-	c.Visit("https://mercari.com/search/?keyword=lego")
-
+	c.Visit("https://www.mercari.com/search/")
 }
 
 func urlBuilderQuery(searchTerm string) *url.URL {
-	baseUrl, err := url.Parse("https://mercari.com/search/")
+	baseUrl, err := url.Parse("https://www.mercari.com/search/")
 	if err != nil {
 		fmt.Println("Malformed URL: ", err.Error())
 	}
