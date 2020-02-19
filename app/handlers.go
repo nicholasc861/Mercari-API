87acfc79630c9c04b5ef18d95f04ed273b226aa8
nicholasc861 <31120213+nicholasc861@users.noreply.github.com>
2020-02-18 23:47:14 -0500

    minor change

--- app/handlers.go
+++ app/handlers.go
@@ -3,8 +3,12 @@ package routes
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"github.com/gorilla/mux"
+	"net/url"
+	"strings"
+
+	"github.com/gocolly/colly"
 )
 
 type Item struct {
@@ -47,4 +51,46 @@ func GetProductsByKeyword(res http.ResponseWriter, req *http.Request) {
 // GET /product/{id}
 func GetProductById(res http.ResponseWriter, req *http.Request) {
 
+}
+
+func fetch_all_items() {
+	c := colly.NewCollector(
+	//colly.Async(true),
+	)
+
+	c.OnHTML("div.Flex-ych44r-0.Space-cutht5-0.Container-sc-9aa7mx-0.hepKGV", func(e *colly.HTMLElement) {
+		e.ForEach("div.Flex-ych44r-0.Space-cutht5-0.Container-sc-9aa7mx-0.withMetaInfo__FullContainer-sc-1j2k5ln-0.hyLExl", func(_ int, e *colly.HTMLElement) {
+			dat := e.ChildText("script")
+
+			jsonData := dat[strings.Index(dat, "{"):len(dat)]
+			i := &Item{}
+			err := json.Unmarshal([]byte(jsonData), i)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			name := i.ItemDesc
+
+			fmt.Printf("Product Name: %s \n", name)
+		})
+	})
+
+	//test := "Nike"
+	//test1 := urlBuilderQuery(test).String()
+	c.Visit("https://www.mercari.com/search/")
+
+}
+
+func urlBuilderQuery(searchTerm string) *url.URL {
+	baseUrl, err := url.Parse("https://www.mercari.com/search/")
+	if err != nil {
+		fmt.Println("Malformed URL: ", err.Error())
+	}
+
+	q := baseUrl.Query()
+	q.Set("keyword", searchTerm)
+	baseUrl.RawQuery = q.Encode()
+
+	return baseUrl
 }
--- main.go
+++ main.go
@@ -1,61 +1,12 @@
 package routes
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/url"
-	"strings"
-	"github.com/gocolly/colly"
-	"github.com/gorilla/mux"
-
+	"net/http"
 )
 
 func main() {
-	fetch_all_items()
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
 
-}
-
-func fetch_all_items() {
-	c := colly.NewCollector(
-	//colly.Async(true),
-	)
-
-	c.OnHTML("div.Flex-ych44r-0.Space-cutht5-0.Container-sc-9aa7mx-0.hepKGV", func(e *colly.HTMLElement) {
-		e.ForEach("div.Flex-ych44r-0.Space-cutht5-0.Container-sc-9aa7mx-0.withMetaInfo__FullContainer-sc-1j2k5ln-0.hyLExl", func(_ int, e *colly.HTMLElement) {
-			dat := e.ChildText("script")
-
-			jsonData := dat[strings.Index(dat, "{"):len(dat)]
-			i := &Item{}
-			err := json.Unmarshal([]byte(jsonData), i)
-
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			name := i.ItemDesc
-
-			fmt.Printf("Product Name: %s \n", name)
-		})
-	})
-
-	//test := "Nike"
-	//test1 := urlBuilderQuery(test).String()
-	c.Visit("https://www.mercari.com/search/")
-
-}
-
-func urlBuilderQuery(searchTerm string) *url.URL {
-	baseUrl, err := url.Parse("https://www.mercari.com/search/")
-	if err != nil {
-		fmt.Println("Malformed URL: ", err.Error())
-	}
-
-	q := baseUrl.Query()
-	q.Set("keyword", searchTerm)
-	baseUrl.RawQuery = q.Encode()
-
-	return baseUrl
 }
